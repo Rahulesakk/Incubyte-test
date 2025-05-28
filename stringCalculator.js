4ee08eecb686d3873809c03c307104aaab19db26
Rahulesakk <esakkanavar98@gmail.com>
2025-05-28 21:59:31 +0530

    result: for delimiter

--- stringCalculator.js
+++ stringCalculator.js
@@ -3,7 +3,14 @@
 function add(numbers){
     if(numbers == '') return 0 
     
-    const numList = numbers.split(",").map(n=>parseInt(n,10))
+    let delimiter = /,|\n/;
+    let numberString = numbers
+    const numList = numberString
+    .split(delimiter)
+    .map(n => n.trim())
+    .filter(n => n !== "")
+    .map(n => parseInt(n, 10));
+
     const negative = numList.filter(n=>n<0)
     if(negative.length>0){
         throw new Error('Negative Numbers not allowed '+negative.join(','))
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -9,6 +9,9 @@ test("returns number for a single Number", () => {
 test("add's two comma-separated numbers", () => {
     expect(add('1,3')).toBe(4);
 })
+test('adds numbers with newline and comma separators', () => {
+    expect(add("1\n2,4")).toBe(7);
+})
 test('throws error on single negative number', () => {
     expect(() => add("1,-2")).toThrow("Negative Numbers not allowed -2");
 });
