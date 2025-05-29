1b170d724ba40b9549b142512f042e0a0a7e1a9d
Rahulesakk <esakkanavar98@gmail.com>
2025-05-29 10:40:46 +0530

    final code for the assessment

--- stringCalculator.js
+++ stringCalculator.js
@@ -4,24 +4,24 @@ function add(numbers){
     if(numbers == '') return 0 
     
     let delimiter = /,|\n/;
-    let numberString = numbers
+    let number_string = numbers
 
     if (numbers.startsWith("//")) {
         const parts = numbers.split("\n");
         delimiter = new RegExp(parts[0].substring(2));
-        numberString = parts.slice(1).join("\n");
+        number_string = parts.slice(1).join("\n");
     }
-    
-    const numList = numberString
+
+    const numb = number_string
     .split(delimiter)
     .map(n => n.trim())
     .filter(n => n !== "")
     .map(n => parseInt(n, 10));
 
-    const negative = numList.filter(n=>n<0)
-    if(negative.length>0){
-        throw new Error('Negative Numbers not allowed '+negative.join(','))
+    const negative_numbers = numb.filter(n=>n<0)
+    if(negative_numbers.length>0){
+        throw new Error('Negative Numbers not allowed '+negative_numbers.join(','))
     }
-    return numList.reduce((sum,n)=>sum+n,0)
+    return numb.reduce((sum,n)=>sum+n,0)
 }      
 module.exports = { add }
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -9,15 +9,16 @@ test("returns number for a single Number", () => {
 test("add's two comma-separated numbers", () => {
     expect(add('1,3')).toBe(4);
 })
-test('adds numbers with newline and comma separators', () => {
-    expect(add("1\n2,4")).toBe(7);
-})
-test('supports custom delimiter', () => {
-    expect(add("//;\n1;2")).toBe(3);
-  });
 test('throws error on single negative number', () => {
     expect(() => add("1,-2")).toThrow("Negative Numbers not allowed -2");
 });
 test('throws error with all negative numbers listed', () => {
     expect(() => add("-1,-2,3")).toThrow("Negative Numbers not allowed -1,-2");
   });
+test('adds numbers with newline and comma separators', () => {
+    expect(add("1\n2,4")).toBe(7);
+})
+test('supports custom delimiter', () => {
+    expect(add("//;\n1;2")).toBe(3);
+  });
+
